fa89ee0c73b261f57d612abe973b7e7eb3eea357
anik <anikbhadra01@gmail.com>
2022-05-21 09:51:47 +0530

    02_counter add todo part done.

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -6,22 +6,20 @@ const value = document.getElementById("value");
 const btns = document.querySelectorAll(".btn");
 
 // todo 01: use arrow function
-btns.forEach(function (btn) {
+btns.forEach((btn) => {
     btn.addEventListener('click', function (e) {
         const styles = e.currentTarget.classList;
 
         // TODO 02: use ternary operator
-        if (styles.contains("increase")) count++;
-        else if (styles.contains("decrease")) count--;
-        else count = 0;
+        styles.contains("increase") ? count++ :
+            (styles.contains("decrease")) ? count-- :
+                count = 0;
         value.textContent = count;
-
         // TODO 03: use ternary operator
         //  change the color of the value wrt buttons
 
-        if (count > 0) value.style.color = "green";
-        else if (count < 0) value.style.color = "red";
-        else value.style.color = "black";
-
+        value.style.color = count > 0 ? "green" :
+            count < 0 ? "red" :
+                "black";
     });
 })
