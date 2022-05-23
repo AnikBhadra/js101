ca7808d75f142f6ef55b059dc60995eb5a6cd5af
anik <anikbhadra01@gmail.com>
2022-05-23 18:51:23 +0530

    01_color-flipper hex.js and app.js exercise todo done.

--- 01_color-fliper/app.js
+++ 01_color-fliper/app.js
@@ -1,13 +1,15 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "yellow", "blue", "violet"];
 
 const btn = document.getElementById("btn");
 // TODO 1: use "getElementsByClassName" instead of "querySelector" and make changes accordingly
 
-const color = document.querySelector(".color");
+// const color = document.querySelector(".color");
+const color = document.getElementsByClassName("color")[0];
 
 // TODO 2: use arrow function in function definitions
 
-btn.addEventListener("click", function () {
+// btn.addEventListener("click", function () 
+btn.addEventListener("click", () => {
     // get random number between 0-3
     const randomNumber = getRandomNumber();
     console.log(randomNumber);
